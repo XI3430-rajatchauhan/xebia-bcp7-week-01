7634b34c2d919456f8903006ec2e7242515b04e6
XI3430-rajatchauhan <99252202+XI3430-rajatchauhan@users.noreply.github.com>
2022-02-11 11:16:39 +0530

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,21 @@
+
+public class Queue {
+	int queue[]=new int[5];
+	int size;
+	int front;
+	int rear;
+	
+	public void enQueue(int data) {
+		queue[rear]=data;
+		rear=rear+1;
+		size=size+1;
+	}
+	
+	public void show() {
+		System.out.println("Elements : ");
+		for (int i=0;i<size;i++) {
+			System.out.print(queue[i] + " ");
+		}
+	}
+
+}
--- Runner.java
+++ Runner.java
@@ -2,16 +2,11 @@
 public class Runner {
 
 	public static void main(String[] args) {
-		Stack nums=new Stack();
-		nums.push(15);
-		nums.push(8);
-		
-		System.out.println(nums.peek());
-		nums.push(10);
-		
-		System.out.println(nums.pop());
-		
-		nums.show();
+		Queue q=new Queue();
+		q.enQueue(5);
+		q.enQueue(2);
+
+		q.show();
 
 	}
 
